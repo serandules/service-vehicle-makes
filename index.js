e053b1983938f7055936991d1b3e92f5f0d52ae1
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2017-08-19 01:32:07 +0530

    updating

--- index.js
+++ index.js
@@ -28,13 +28,10 @@ module.exports = function (router) {
     router.use(serandi.pond);
     router.use(serandi.ctx);
     router.use(auth({
-        GET: {
-            open: [
-                '^\/$',
-                '^\/.*'
-            ],
-            hybrid: []
-        }
+        GET: [
+            '^\/$',
+            '^\/.*'
+        ]
     }));
     router.use(bodyParser.json());
 
