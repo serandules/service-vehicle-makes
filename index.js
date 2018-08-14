2d53bb4de02bf5d4df9c8ae6483c2295ea7a28a3
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-08-14 10:05:43 +0530

    updating

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ module.exports = function (router) {
         ]
     }));
     router.use(throttle.apis('vehicle-makes'));
-    router.use(serandi.cors);
     router.use(bodyParser.json());
 
     /**
